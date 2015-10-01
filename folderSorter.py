846712d7113897b93373ceaa7984a570d479db9d
Alexander Hermstad <hermstadalex@gmail.com>
2015-10-01 11:21:20 -0700

    user input functionallity complete

--- folderSorter.py
+++ folderSorter.py
@@ -2,45 +2,62 @@ import os
 import time
 import shutil
 
-def createFolders():
-    if not os.path.exists("./CurrentFiles"):
-        os.makedirs('CurrentFiles')
-    if not os.path.exists("./OlderFiles"):
-        os.makedirs('OlderFiles')
 
-def unpackFolders():
+def unpackFolders(newFolderName, oldFolderName):
     moveToLoc = os.getcwd()
-    os.chdir('OlderFiles')
+    os.chdir(oldFolderName)
     files = os.listdir('.')
     for file in files:
         shutil.move(file, moveToLoc)
     os.chdir(moveToLoc)
-    os.chdir('CurrentFiles')
+    os.chdir(newFolderName)
     files = os.listdir('.')
     for file in files:
         shutil.move(file, moveToLoc)
+    os.chdir(moveToLoc)
 
+def getUserInput(folderMode):
+    if folderMode != True:
+        timeSplitterDays = int(raw_input("Enter the number of days you want to" +
+        " distinguish between current and old: "))
+        return timeSplitterDays
+    else:
+        newFolderName = str(raw_input("Enter the name of the folder with new files:"
+        " "))
+        oldFolderName = str(raw_input("Enter the name of the folder with old files:"
+        " "))
+        return  (newFolderName, oldFolderName)
 
 def folderSorter():
-    timeSplitter = 60*60*24*30 # Time value at which files are separated
-    createFolders()
+    timeSplitterDays = getUserInput(False)
+    timeSplitter = 60 * 60 * 24 * timeSplitterDays # Time value at which files are separated
+    newFolderName, oldFolderName = createFolders()
     files = os.listdir('.')
 
     for file in files:
-        if (file != 'CurrentFiles') and (file != 'OlderFiles') and (file !=
+        if (file != newFolderName) and (file != oldFolderName) and (file !=
         '.git') and (file != 'folderSorter.py') and (file != 'README.md'):
             fileSecondsExisted = time.time() - os.path.getctime(file)
             if fileSecondsExisted >= timeSplitter:
-                shutil.move(file, 'OlderFiles')
+                shutil.move(file, oldFolderName)
             elif fileSecondsExisted < timeSplitter:
-                shutil.move(file, 'CurrentFiles')
+                shutil.move(file, newFolderName)
             else:
                 print "Error has occured"
                 return
-    
+
+def createFolders():
+    newFolder, oldFolder = getUserInput(True)
+    print newFolder
+    print oldFolder
+    if not os.path.exists(newFolder):
+        os.makedirs(newFolder)
+    if not os.path.exists(oldFolder):
+        os.makedirs(oldFolder)
+    return newFolder, oldFolder
 
 
 def main():
-    unpackFolders()
+    folderSorter()
 
 main()
